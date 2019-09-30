eb18730b0345f107e61288b6b258645b1ffbeb66
Diana Dale <diana.drulle@gmail.com>
2019-09-30 20:56:34 +0300

    Logic and structure of the code

---
+++ Exceptions/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Calculator
+    {
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static decimal Divide(int num1, int num2)
+        {
+            //rules
+            if(num2 == 0)
+            {
+                throw new CalculatorExceptions("Nevar dalīt ar O");
+            } //exceprion ir pedejais kas nostrada lidz catch blokam
+            return (decimal)num1 / num2;
+           //execution
+        }
+    }
+}
--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -10,6 +10,42 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
+            try
+            {
+            int num1 = GetNumber("Enter number 1: ");
+            int num2 = GetNumber("Enter number 2: ");
+
+            Console.WriteLine("Division is {0}", Calculator.Divide(num1, num2));
+            }
+            catch(CalculatorExceptions ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Neparedzēta kļūda!");
+            }
+            Console.Read();
+        }
+        static int GetNumber(string text)
+        {
+            Console.Write(text);
+            string input = Console.ReadLine();
+
+            if(int.TryParse(input, out int result))
+            {
+                return result;
+            }
+            try
+            {
+                int result1 = int.Parse(input);
+                return result;
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Invalid number!");
+                return GetNumber(text);
+            }
         }
     }
 }
